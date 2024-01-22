4607c4cb3cc504c435e70191d4006599251f20fd
Acer-User <Acer-User@DESKTOP-NSD4PLV>
2024-01-22 17:36:16 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_7_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //use write not writeline 
+            Console.Write("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("Hi" +  name);
+            Console.Write("How old are you? ");
+            //the values from user are always string
+            string age = Console.ReadLine();
+            Console.WriteLine("you are " + age);
+
+            Console.ReadLine();
+
+        }
+    }
+}
